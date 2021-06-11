6124f538d8dd54f5cf7168e1e49a1a9c29086ec8
ike04 <boc.0409@outlook.jp>
2021-06-11 16:12:17 +0900

    importの修正

--- app/src/main/java/com/google/codelab/musiclibrary/view/MainActivity.kt
+++ app/src/main/java/com/google/codelab/musiclibrary/view/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.google.codelab.musiclibrary.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.codelab.musiclibrary.R
 import com.google.codelab.musiclibrary.databinding.ActivityMainBinding
@@ -10,23 +10,25 @@ import com.google.codelab.musiclibrary.ext.FragmentExt.showFragment
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val searchFragment = SearchFragment()
-    private val rankingFragment= RankingFragment()
+    private val rankingFragment = RankingFragment()
 
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_search -> {
-                searchFragment.showFragment(supportFragmentManager)
-                supportActionBar?.hide()
-                true
+    private val onNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_search -> {
+                    searchFragment.showFragment(supportFragmentManager)
+                    supportActionBar?.hide()
+                    true
+                }
+                R.id.navigation_ranking -> {
+                    rankingFragment.showFragment(supportFragmentManager)
+                    supportActionBar?.show()
+                    true
+                }
+                else -> false
             }
-            R.id.navigation_ranking -> {
-                rankingFragment.showFragment(supportFragmentManager)
-                supportActionBar?.show()
-                true
-            }
-            else -> false
         }
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
--- app/src/main/java/com/google/codelab/musiclibrary/view/SearchFragment.kt
+++ app/src/main/java/com/google/codelab/musiclibrary/view/SearchFragment.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.codelab.musiclibrary.R
-import com.google.codelab.musiclibrary.model.Song
 import com.google.codelab.musiclibrary.databinding.FragmentSearchBinding
+import com.google.codelab.musiclibrary.model.Song
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 
