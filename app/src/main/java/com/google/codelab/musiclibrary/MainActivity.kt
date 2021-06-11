316c0ab35d82b863a9b4e22637d71fd9bdf15cc6
ike04 <boc.0409@outlook.jp>
2021-06-11 12:13:32 +0900

    BottomNavigationの実装

--- app/src/main/java/com/google/codelab/musiclibrary/MainActivity.kt
+++ app/src/main/java/com/google/codelab/musiclibrary/MainActivity.kt
@@ -2,10 +2,32 @@ package com.google.codelab.musiclibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.codelab.musiclibrary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        when (item.itemId) {
+            R.id.navigation_search -> {
+
+                true
+            }
+            R.id.navigation_ranking -> {
+
+                true
+            }
+        }
+        false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        setContentView(binding.root)
     }
 }
